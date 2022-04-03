2d37e852fcac299eca187735f1a8ce280e8db883
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-04 03:41:53 +0600

    charts added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,99 @@
 import React from 'react';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    PieChart,
+    Pie
+} from "recharts";
+
+const data = [
+    {
+        "month": "Mar",
+        "investment": 100000,
+        "sell": 241,
+        "revenue": 10401
+    },
+    {
+        "month": "Apr",
+        "investment": 200000,
+        "sell": 423,
+        "revenue": 24500
+    },
+    {
+        "month": "May",
+        "investment": 500000,
+        "sell": 726,
+        "revenue": 67010
+    },
+    {
+        "month": "Jun",
+        "investment": 500000,
+        "sell": 529,
+        "revenue": 40405
+    },
+    {
+        "month": "Jul",
+        "investment": 600000,
+        "sell": 601,
+        "revenue": 50900
+    },
+    {
+        "month": "Aug",
+        "investment": 700000,
+        "sell": 670,
+        "revenue": 61000
+    }
+];
+
 
 const Dashboard = () => {
-
     return (
         <div>
-            <h3>This is Dashboard</h3>
+            <div>
+                <LineChart width={500} height={300} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" padding={{ left: 30, right: 30 }} />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                        type="monotone"
+                        dataKey="sell"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                    />
+                </LineChart>
+            </div>
+            <div>
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={data}
+                        dataKey="investment"
+                        cx={200}
+                        cy={200}
+                        outerRadius={60}
+                        fill="#8884d8"
+                    />
+                    <Pie
+                        data={data}
+                        dataKey="revenue"
+                        cx={200}
+                        cy={200}
+                        innerRadius={70}
+                        outerRadius={90}
+                        fill="#82ca9d"
+                        label
+                    />
+                </PieChart>
+            </div>
         </div>
     );
 };
+
 
 export default Dashboard;
