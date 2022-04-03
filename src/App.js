32fae5b05758144cf5efcd3e1bc726a936f8f308
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-03 22:57:05 +0600

    added addd components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Review from './components/Review/Review';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
+      <Review></Review>
+      <Dashboard></Dashboard>
+      <Blogs></Blogs>
+      <About></About>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>Text</Col>
+                    <Col>Figure</Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Home;
