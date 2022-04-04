33fcc619881d933d04e4b1b922f53b96dfc3481f
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-04 17:37:00 +0600

    images added in the review card

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import './Review.css'
 const Review = (props) => {
-    const { name, review, rating } = props.review;
+    const { name, review, rating, img } = props.review;
     return (
         <div className='my-5 mx-2'>
             <Card>
-                <Card.Header as="h5">{name}</Card.Header>
+                <Card.Header as="h5"><img className='card-image' src={img} alt="" />{name}</Card.Header>
                 <Card.Body>
                     <Card.Title>{review}</Card.Title>
                     <Card.Text>{rating}</Card.Text>
