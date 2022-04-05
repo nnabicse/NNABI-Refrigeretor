b30458e99b6ca0691a3725cac2df1f2440dcc801
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-05 17:41:45 +0600

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <footer class="footer mt-auto py-3">
+            <div class="container">
+                <span>© 2022 Copyright: Nurun Nabi</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
